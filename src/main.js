ea84ff982b378506dd3280ce29d185e210b52d16
vimillan <20193tn066@utez.edu.mx>
2022-11-24 11:39:28 -0600

    Vuesax config

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import Vuesax from 'vuesax'
+import 'vuesax/dist/vuesax.css'
 
 Vue.config.productionTip = false
+
+Vue.use(Vuesax);
 
 new Vue({
   router,
