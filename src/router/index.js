19d158c3eb570536ca8c0c33a6184ad62d2d32a2
vimillan <20193tn066@utez.edu.mx>
2022-11-25 15:51:59 -0600

    feat: Edited route children into default path

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,21 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import ContentLayout from '../layout/ContentLayout.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    component: ContentLayout,
+    children: [
+      {
+        path: "",
+        name: "Home",
+        component: Home,
+      },
+    ],
   },
   {
     path: '/about',
