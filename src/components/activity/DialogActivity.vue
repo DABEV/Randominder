09c12b1c20bfb69b0845041cb7a633b6495ae0ad
vimillan <20193tn066@utez.edu.mx>
2022-11-27 20:04:42 -0600

    feat: added activity dialog

---
+++ src/components/activity/DialogActivity.vue
@@ -0,0 +1,88 @@
+<template>
+  <div>
+    <vs-dialog prevent-close v-model="active" width="400px">
+      <template #header>
+        <h3 class="back-outlined width-80 c-light-blue text-center">
+          Activity
+        </h3>
+      </template>
+
+      <div class="text-center">
+        {{ data.activity }}
+      </div>
+      <vs-row justify="center" aling="center" class="px-1">
+        <vs-col w="6">
+          <vs-row class="center mt-2">
+            <vs-avatar size="30" circle color="#FD9AB6">
+              <i class="bx bx-purchase-tag-alt"></i>
+            </vs-avatar>
+            <div class="pl-1">{{ data.type }}</div>
+          </vs-row>
+          <vs-row class="center mt-2">
+            <vs-avatar size="30" circle color="#BED4F0">
+              <i class="bx bxs-hand"></i>
+            </vs-avatar>
+            <div v-if="data.accessibility <= 0.4" class="pl-1">Easy</div>
+            <div v-else-if="data.accessibility == 0.5" class="pl-1">
+              Mid range
+            </div>
+            <div v-else class="pl-1">Hard</div>
+          </vs-row>
+        </vs-col>
+        <vs-col w="6">
+          <vs-row class="center mt-2">
+            <vs-avatar size="30" circle color="#9ED6E6">
+              <i class="bx bx-dollar-circle"></i>
+            </vs-avatar>
+            <div v-if="data.price <= 0.4" class="pl-1">Free</div>
+            <div v-else-if="data.price == 0.5" class="pl-1">Cheap</div>
+            <div v-else class="pl-1">Expensive</div>
+          </vs-row>
+          <vs-row class="center mt-2">
+            <vs-avatar size="30" circle color="#FEBF1A">
+              <i class="bx bx-user"></i>
+            </vs-avatar>
+            <div class="pl-1">
+              {{ data.participants }}
+            </div>
+          </vs-row>
+        </vs-col>
+      </vs-row>
+      <div v-if="data.link" class="mt-1 text-center">
+        <a :href="data.link">Recurso de apoyo</a>
+      </div>
+
+      <template #footer>
+        <div class="mt-1">
+          <vs-button block floating color="#FD9AB6" @click="reloadSearch">
+            Search again
+          </vs-button>
+        </div>
+      </template>
+    </vs-dialog>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DialogActivity',
+  data: () => ({
+    active: false,
+    activity: {},
+  }),
+  methods: {
+    changeActive() {
+      this.active = !this.active
+    },
+    reloadSearch() {
+      this.active = false
+      this.$emit('reloadSearch')
+    },
+  },
+  props: {
+    data: Object,
+  },
+}
+</script>
+
+<style></style>
--- src/components/activity/SelectActivity.vue
+++ src/components/activity/SelectActivity.vue
@@ -2,30 +2,13 @@
   <div>
     <vs-row justify="space-between">
       <vs-col w="7" class="pa-2 bg-white ma-2 rounded shadow">
-        <vs-row justify="center" aling="center">
-          <vs-col w="6">
-            <h3 class="back-outlined width-80 c-dark-pink text-center">
-              Activities
-            </h3>
-          </vs-col>
-          <vs-col w="3">
-            <vs-button
-              animation-type="rotate"
-              color="#88D6E3"
-              size="large"
-              floating
-            >
-              <i class="bx bx-dice-5"></i>
-              Randomly
-              <template #animate>
-                <i class="bx bxs-dice-5"></i>
-                Randomly
-              </template>
-            </vs-button>
-          </vs-col>
-        </vs-row>
+        <h3 class="back-outlined width-40 c-light-purple text-center">
+          Activities
+        </h3>
+        <div class="mt-1">
+          You can filter activities or get one randomly
+        </div>
         <FilterActivity />
-        <TypeList />
       </vs-col>
       <vs-col w="4" class="pa-2 bg-white ma-2 rounded shadow">
         <Favs />
@@ -43,5 +26,6 @@ export default {
   components: { TypeList, FilterActivity, Favs },
   name: 'SelectActivity',
   data: () => ({}),
+  methods: {},
 }
 </script>
