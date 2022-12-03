0faa8ac29520cfb430af3e6c15bcd4fce26de516
vimillan <20193tn066@utez.edu.mx>
2022-12-02 22:20:22 -0600

    feat: Added navbar

---
+++ src/components/Navbar.vue
@@ -0,0 +1,71 @@
+<template>
+  <div>
+    <vs-navbar
+      text-white
+      center-collapsed
+      v-model="active"
+      color="#88D6E3"
+      class="pa-0"
+    >
+      <template #left>
+        <img
+          class="img-nav"
+          src="https://firebasestorage.googleapis.com/v0/b/pret-img.appspot.com/o/randominder%2Fmedals%2Flogo-randominder.svg?alt=media&token=64d3e5b5-7e04-47b9-861c-8ac79335e855"
+          alt=""
+        />
+        <strong class="ml-1 c-white">Randominder</strong>
+      </template>
+      <vs-navbar-item
+        :active="active == 'home'"
+        id="home"
+        to="/"
+        class="c-white"
+      >
+        Home
+      </vs-navbar-item>
+      <vs-navbar-item
+        :active="active == 'activities'"
+        id="activities"
+        to="/activities"
+        class="c-white"
+      >
+        Activities
+      </vs-navbar-item>
+      <vs-navbar-item
+        :active="active == 'recipes'"
+        id="recipes"
+        to="/recipes"
+        class="c-white"
+      >
+        Recipes
+      </vs-navbar-item>
+      <vs-navbar-item
+        :active="active == 'trivia'"
+        id="trivia"
+        to="/trivia"
+        class="c-white"
+      >
+        Trivia
+      </vs-navbar-item>
+      <template #right>
+        <vs-button
+          color="#FF99B4"
+          floating
+          to="/activities"
+          @click="active = 'activities'"
+        >
+          Get Started
+        </vs-button>
+      </template>
+    </vs-navbar>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Navbar',
+  data: () => ({
+    active: '',
+  }),
+}
+</script>
--- src/layout/ContentLayout.vue
+++ src/layout/ContentLayout.vue
@@ -1,22 +1,26 @@
 <template>
-  <div>
-    <div>
+  <div class="bg-blue-back">
+    <navbar></navbar>
+    <br />
+    <div class="mt-2">
       <router-view></router-view>
     </div>
   </div>
 </template>
 
 <script>
+import Navbar from '../components/Navbar.vue'
 export default {
-  name: "ContentLayout",
-};
+  components: { Navbar },
+  name: 'ContentLayout',
+}
 </script>
 
 <style>
-@import "../assets/css/content.css";
-@import "../assets/css/spacing.css";
-@import "../assets/css/colors.css";
-@import "../assets/css/background.css";
-@import "../assets/css/typography.css";
-@import "../assets/css/aligment.css";
+@import '../assets/css/content.css';
+@import '../assets/css/spacing.css';
+@import '../assets/css/colors.css';
+@import '../assets/css/background.css';
+@import '../assets/css/typography.css';
+@import '../assets/css/aligment.css';
 </style>
